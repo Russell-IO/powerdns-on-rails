425cf1893edbd28ce5fe528d6e90db753194b8c1
Alexander E. Fischer <aef@raxys.net>
2009-07-15 23:54:18 +0200

    Added support for SSHFP records (RFC 4255)

--- app/models/record.rb
+++ app/models/record.rb
@@ -24,7 +24,7 @@ class Record < ActiveRecord::Base
   after_save  :update_soa_serial
 
   # Known record types
-  @@record_types = ['A', 'AAAA', 'CNAME', 'LOC', 'MX', 'NS', 'PTR', 'SOA', 'SPF', 'SRV', 'TXT']
+  @@record_types = ['A', 'AAAA', 'CNAME', 'LOC', 'MX', 'NS', 'PTR', 'SOA', 'SPF', 'SRV', 'SSHFP', 'TXT']
   cattr_reader :record_types
 
   class << self
