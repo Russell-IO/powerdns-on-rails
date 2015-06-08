3f2b4297589b637d661bd3d7576c5814c63aba1b
Herman verschooten <Herman@verschooten.net>
2015-06-08 14:19:17 +0200

    Add HMAIL to route outgoing mails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,13 @@ PowerdnsOnRails::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method =:smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    address: "mail.hmail.be",
+    enable_starttls_auto: true
+  }
 
   # Enable threaded mode
   # config.threadsafe!
